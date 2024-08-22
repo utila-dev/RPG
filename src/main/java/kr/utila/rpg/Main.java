3fa12446b08d4a874fb029fd7d490469a2712a7c
BuildTools <unconfigured@null.spigotmc.org>
2024-08-23 00:23:58 +0900

    initial commit

---
+++ src/main/java/kr/utila/rpg/Main.java
@@ -0,0 +1,14 @@
+package kr.utila.rpg;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
